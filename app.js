5fb5327f132814174a829fa8663af721d4c61dd4
Evgeniya <jane.karell19@gmail.com>
2022-02-07 12:35:57 +0000

    complete responsive header with burger effects and title

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_list');
+    const navLinks = document.querySelectorAll('.nav_link');
+
+    burger.addEventListener('click', () => {
+        // Toggle nav
+        nav.classList.toggle('nav_active');
+        // Animate links
+        navLinks.forEach((link, index) => {
+            link.style.animation
+            ? link.style.animation = ''
+            : link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        });
+        // Burger animation
+        burger.classList.toggle('toggle');
+    
+    
+    
+    
+    
+    
+    
+    
+    });
+}
+
+navSlide();
+
