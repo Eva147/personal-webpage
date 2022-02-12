9cf1139899d22c96a52bbfad7b46048ff247ce34
Evgeniya <eva147@list.ru>
2022-02-12 07:40:39 +0000

    Add animation for About page title

--- app.js
+++ app.js
@@ -26,15 +26,14 @@ const navSlide = () => {
 }
 navSlide();
 
-// Animation for Titles
-const tl = gsap.timeline({defaults: {ease: "expo.out"}});
-tl.to(".animated_title, .animated_par", {opacity: 1, duration: 2.5, stagger: .5});
-tl.to(".animated_title, .animated_par", {y: "0%", duration: 2.5, stagger: .5}, "-=3");
-tl.fromTo("nav", {opacity: "0"}, {opacity:"1", duration: 1}, "-=2");
+// Animation for Main title and nav bar
+const tl_main = gsap.timeline({defaults: {ease: "expo.out"}});
+tl_main.to(".animated_title, .animated_par", {opacity: 1, duration: 2.5, stagger: .5});
+tl_main.to(".animated_title, .animated_par", {y: "0%", duration: 2.5, stagger: .5}, "-=3");
+tl_main.fromTo("nav", {opacity: "0"}, {opacity:"1", duration: 1}, "-=2");
 
-// const tl2 = gsap.timeline({defaults: {ease: "expo.out"}});
-// tl.to(".animated_title, .animated_par", {opacity: 1, duration: 2.5, stagger: .5});
-// tl.to(".animated_title, .animated_par", {y: "0%", duration: 2.5, stagger: .5}, "-=3");
-// tl.fromTo("nav", {opacity: "0"}, {opacity:"1", duration: 1}, "-=2");
-
+// Animation for About title
+const tl_about = gsap.timeline({defaults: {ease: "expo.out"}});
+tl_about.to(".animated_about-title", {opacity: 1, duration: 3, stagger: .5});
+tl_about.to(".animated_about-title", {y: "0%", duration: 2.5, stagger: .5}, "-=3");
 
