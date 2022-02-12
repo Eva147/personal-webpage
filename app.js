2e55d7b9bacf05cc5c9f17bb25bc0a17a9b46e49
Evgeniya <eva147@list.ru>
2022-02-12 07:22:36 +0000

    Add animation for Title and nav on the main page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.getElementById("curYear").innerHTML=currentYear;
 console.log(currentYear);
         
 
-// Burger animation
+// Animation for Burger and nav bar in mobile version
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav_list');
@@ -24,6 +24,17 @@ const navSlide = () => {
         burger.classList.toggle('toggle');
     });
 }
-
 navSlide();
+
+// Animation for Titles
+const tl = gsap.timeline({defaults: {ease: "expo.out"}});
+tl.to(".animated_title, .animated_par", {opacity: 1, duration: 2.5, stagger: .5});
+tl.to(".animated_title, .animated_par", {y: "0%", duration: 2.5, stagger: .5}, "-=3");
+tl.fromTo("nav", {opacity: "0"}, {opacity:"1", duration: 1}, "-=2");
+
+// const tl2 = gsap.timeline({defaults: {ease: "expo.out"}});
+// tl.to(".animated_title, .animated_par", {opacity: 1, duration: 2.5, stagger: .5});
+// tl.to(".animated_title, .animated_par", {y: "0%", duration: 2.5, stagger: .5}, "-=3");
+// tl.fromTo("nav", {opacity: "0"}, {opacity:"1", duration: 1}, "-=2");
+
 
