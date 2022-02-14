b4654d14d6a3798616883737e70e2186aa3a1058
Evgeniya <eva147@list.ru>
2022-02-14 15:42:25 +0000

    Add animation for texts on pages

--- app.js
+++ app.js
@@ -37,3 +37,26 @@ const tl_about = gsap.timeline({defaults: {ease: "expo.out"}});
 tl_about.to(".animated_about-title", {opacity: 1, duration: 3, stagger: .5});
 tl_about.to(".animated_about-title", {y: "0%", duration: 2.5, stagger: .5}, "-=3");
 
+// Animation for all texts
+
+function animateFromTo(elem) {
+
+    gsap.fromTo(elem, {y: "30px", opacity: 0, autoAlpha: 0}, {
+      duration: 2, 
+      y: 0,
+      opacity: 1,
+      autoAlpha: 1, 
+      ease: "expo.out", 
+      overwrite: "auto"
+    });
+  }
+
+document.addEventListener("DOMContentLoaded", function() {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.utils.toArray("._activeText").forEach(function(elem) {
+        ScrollTrigger.create({
+          trigger: elem,
+          onEnter: function() { animateFromTo(elem) }
+        });
+    });
+})
