093e134e5346dfdab03efb40b7fb2083422e14f4
Evgeniya <eva147@list.ru>
2022-07-20 20:06:05 +0100

    Fix issues with scroll event

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Animation for Navbar change colour
 gsap.registerPlugin(ScrollTrigger);
-
+// time line for crossing middle container
 const tl_nav = gsap.timeline ({
   scrollTrigger: {
     trigger: '.middle-container',
@@ -8,56 +8,45 @@ const tl_nav = gsap.timeline ({
     toggleActions: "restart none none reverse",
     },
 });
-tl_nav.to('header', {backgroundColor: '#FFE7D1', duration: 0.5});
-tl_nav.to('#logo_name h3, .nav_link', {color: '#396362', fontWeight: 400, opacity: 1, duration: 0.5}, '-=1');
+tl_nav.to('header, .nav_items', {backgroundColor: '#FFE7D1', duration: 0.5});
+tl_nav.to('.burger div', {backgroundColor: '#396362', duration: 0.3})
+tl_nav.to('.nav_link', {color: '#396362', fontWeight: 400, opacity: 1, duration: 0.5}, '-=1');
+
 
 
 // Hide Navbar on scroll
 const showAnim = gsap.from('header', {
   yPercent: -100,
   paused: true,
-  duration: 0.5
-  }).progress(1);
-  ScrollTrigger.create({
-    start: "top top",
-    onUpdate: (self) => {
-      self.direction === -1 ? showAnim.play() : showAnim.reverse();
-    }
-  });
-
+  duration: 0.3
+}).progress(1);
+ScrollTrigger.create({
+  start: "top top",
+  onUpdate: (self) => {
+    self.direction === -1 ? showAnim.play() : showAnim.reverse();
+  }
+});
 
 // Animation for Burger and nav bar in mobile version
 function navSlide() {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav_items');
-    const navLinks = document.querySelectorAll('.nav_link');
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav_items');
+  const navLinks = document.querySelectorAll('.nav_link');
 
-    burger.addEventListener('click', () => {
-      // Toggle nav
-      nav.classList.toggle('nav_active');
-      // Animate links
-      navLinks.forEach((link, index) => {
-        link.style.animation
-            ? link.style.animation = ''
-            : link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-      });
-      // Burger animation
-      burger.classList.toggle('toggle');
-    });
+  // SHOW menu on CLICK
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav_active');
+    burger.classList.toggle('toggle');
 
-    // Hide a menu on scroll
-    document.addEventListener('scroll', () => {
-      burger.classList.remove('toggle');
-      nav.classList.remove('nav_active');
-      navLinks.forEach((link, index) => {link.style.animation= ''});
-    });
+  });
+
+  // HIDE menu on SCROLL
+  document.addEventListener('scroll', () => {
+    burger.classList.remove('toggle');
+    nav.classList.remove('nav_active');
+  });
 };
 navSlide();
-
-
-
-// Change colour of the menu on scroll
-
 
 // Animation for Main title and nav bar
 const tl_main = gsap.timeline({defaults: {ease: "expo.out"}});
@@ -79,31 +68,25 @@ if (animatedAboutTitle) {
 
 // Animation for all texts
 function animateText(elem) {
-
-    gsap.fromTo(elem, {y: "30px", opacity: 0, autoAlpha: 0}, {
-      duration: 3, 
-      y: 0,
-      opacity: 1,
-      autoAlpha: 1, 
-      ease: "expo.out", 
-      overwrite: "auto"
-    });
+  gsap.fromTo(elem, {y: "30px", opacity: 0, autoAlpha: 0}, {
+    duration: 3, 
+    y: 0,
+    opacity: 1,
+    autoAlpha: 1, 
+    ease: "expo.out", 
+    overwrite: "auto"
+  });
 }
-
+// all text elements 
 document.addEventListener("DOMContentLoaded", function() {
-
-    // all text elements 
-    gsap.utils.toArray("._activeText").forEach(function(elem) {
-        ScrollTrigger.create({
-          trigger: elem,
-          onEnter: function() { animateText(elem) }
-        });
-    });
+  gsap.utils.toArray("._activeText").forEach(function(elem) {
+      ScrollTrigger.create({
+        trigger: elem,
+        onEnter: function() { animateText(elem) }
+      });
+  });
 });
-
-
 // Animation for Monograms
-
 function offset(el) {
     const rect = el.getBoundingClientRect(),
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -129,7 +112,6 @@ if (animItems.length > 0) {
     }
   }
 }
-
 
 // Current year for Footer
 let date = new Date();
