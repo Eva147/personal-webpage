fb065c38c5ee8aab9ffb4bc2e58cef51b7d7f16b
Evgeniya <eva147@list.ru>
2022-07-20 14:02:01 +0100

    Add logos, animate main logo, add animation to burger and pop up menu

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-       
 // Animation for Navbar change colour
 gsap.registerPlugin(ScrollTrigger);
 
 const tl_nav = gsap.timeline ({
-    scrollTrigger: {
-        trigger: '.middle-container',
-        start: "-7% top",
-        toggleActions: "restart none none reverse",
-        },
+  scrollTrigger: {
+    trigger: '.middle-container',
+    start: "-7% top",
+    toggleActions: "restart none none reverse",
+    },
 });
 tl_nav.to('header', {backgroundColor: '#FFE7D1', duration: 0.5});
 tl_nav.to('#logo_name h3, .nav_link', {color: '#396362', fontWeight: 400, opacity: 1, duration: 0.5}, '-=1');
@@ -15,9 +14,9 @@ tl_nav.to('#logo_name h3, .nav_link', {color: '#396362', fontWeight: 400, opacit
 
 // Hide Navbar on scroll
 const showAnim = gsap.from('header', {
-    yPercent: -100,
-    paused: true,
-    duration: 0.5
+  yPercent: -100,
+  paused: true,
+  duration: 0.5
   }).progress(1);
   ScrollTrigger.create({
     start: "top top",
@@ -26,6 +25,7 @@ const showAnim = gsap.from('header', {
     }
   });
 
+
 // Animation for Burger and nav bar in mobile version
 function navSlide() {
     const burger = document.querySelector('.burger');
@@ -33,30 +33,49 @@ function navSlide() {
     const navLinks = document.querySelectorAll('.nav_link');
 
     burger.addEventListener('click', () => {
-        // Toggle nav
-        nav.classList.toggle('nav_active');
-        // Animate links
-        navLinks.forEach((link, index) => {
-            link.style.animation
-                ? link.style.animation = ''
-                : link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-        });
-        // Burger animation
-        burger.classList.toggle('toggle');
+      // Toggle nav
+      nav.classList.toggle('nav_active');
+      // Animate links
+      navLinks.forEach((link, index) => {
+        link.style.animation
+            ? link.style.animation = ''
+            : link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      });
+      // Burger animation
+      burger.classList.toggle('toggle');
     });
-}
+
+    // Hide a menu on scroll
+    document.addEventListener('scroll', () => {
+      burger.classList.remove('toggle');
+      nav.classList.remove('nav_active');
+      navLinks.forEach((link, index) => {link.style.animation= ''});
+    });
+};
 navSlide();
+
+
+
+// Change colour of the menu on scroll
+
 
 // Animation for Main title and nav bar
 const tl_main = gsap.timeline({defaults: {ease: "expo.out"}});
-tl_main.to(".animated_title, .animated_par", {opacity: 1, duration: 2.5, stagger: .5});
-tl_main.to(".animated_title, .animated_par", {y: "0%", duration: 3, stagger: .5}, "-=3");
-tl_main.fromTo("nav", {opacity: "0"}, {opacity:"1", duration: 2}, "-=2");
+const animatedTitle = document.querySelector(".animated_title");
+const animatedParagraph = document.querySelector(".animated_par");
+if (animatedTitle || animatedParagraph) {
+  tl_main.to(".animated_title, .animated_par", {opacity: 1, duration: 2.5, stagger: .5});
+  tl_main.to(".animated_title, .animated_par", {y: "0%", duration: 3, stagger: .5}, "-=3");
+  tl_main.fromTo("nav", {opacity: "0"}, {opacity:"1", duration: 2}, "-=2");  
+}
 
 // Animation for About title
 const tl_about = gsap.timeline({defaults: {ease: "expo.out"}});
-tl_about.to(".animated_about-title", {opacity: 1, duration: 3, stagger: .5});
-tl_about.to(".animated_about-title", {y: "0%", duration: 2.5, stagger: .5}, "-=3");
+const animatedAboutTitle = document.querySelector(".animated_about-title");
+if (animatedAboutTitle) {
+  tl_about.to(".animated_about-title", {opacity: 1, duration: 3, stagger: .5});
+  tl_about.to(".animated_about-title", {y: "0%", duration: 2.5, stagger: .5}, "-=3");  
+}
 
 // Animation for all texts
 function animateText(elem) {
@@ -94,26 +113,21 @@ function offset(el) {
 const animItems = document.querySelectorAll(".monogram_svg");
 
 if (animItems.length > 0) {
-    window.addEventListener('scroll', animMonogramOnScroll);
-  
-    function animMonogramOnScroll() {
-      for (let index = 0; index < animItems.length; index++) {
-        const animItem = animItems[index];
-        const animItemHeight = animItem.getAttribute("height");
+  window.addEventListener('scroll', animMonogramOnScroll);
 
-        const animItemOffset = offset(animItem);
-        let animItemPoint = window.innerHeight - animItemHeight * 1.5;
+  function animMonogramOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.getAttribute("height");
 
-        if (scrollY > (animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
-           animItem.classList.add('monogram_active_path'); 
-        } else {
-          if(!animItem.classList.contains('_anim_no_hide')) {
-            animItem.classList.remove('monogram_active_path'); 
-          }
-        } 
+      const animItemOffset = offset(animItem);
+      let animItemPoint = window.innerHeight - animItemHeight * 1.5;
+
+      if (scrollY > (animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+          animItem.classList.add('monogram_active_path'); 
       }
     }
-    
+  }
 }
 
 
