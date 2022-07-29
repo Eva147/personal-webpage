f807463e50319897c3d9b2e2eeb88650bd997524
Evgeniya <eva147@list.ru>
2022-07-29 14:09:08 +0100

    Add responsive carousel, add link to github

--- app.js
+++ app.js
@@ -121,3 +121,32 @@ if (animItems.length > 0) {
 let date = new Date();
 let currentYear = date.getFullYear();
 document.getElementById("curYear").innerHTML=currentYear;
+
+
+// Animation for slideshow 
+let slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
